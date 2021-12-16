280a0a03f897f308b44c283355a6320d56f137dd
Vitor Adriano <vradriano@hotmail.com>
2021-12-16 16:58:22 -0300

    create RepositoryList

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,37 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de Repositórios</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Form in Reactjs</p>
+
+          <a href="#">acessar repositório</a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Form in Reactjs</p>
+
+          <a href="#">acessar repositório</a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Form in Reactjs</p>
+
+          <a href="#">acessar repositório</a>
+        </li>
+
+        <li>
+          <strong>unform</strong>
+          <p>Form in Reactjs</p>
+
+          <a href="#">acessar repositório</a>
+        </li>
+      </ul>
+    </section>
+  )
+}
